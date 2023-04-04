fef67f87226f003e93f879380bbe710aa366103a
Luke Taylor <tekul@pm.me>
2023-04-04 20:57:22 +0100

    Refactor to support both grpc and http otlp
    Configuration is based off standard env variables defined by
    OpenTelemetry.

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,7 @@ use tracing_subscriber::prelude::*;
 async fn main() -> io::Result<()> {
     let fmt_layer = tracing_subscriber::fmt::layer();
 
-    let mut exporter = opentelemetry_otlp::new_exporter()
-        .tonic()
-        .with_metadata(load_metadata_from_env())
-        .with_env();
-
-    // Check if we need TLS
-    if let Ok(endpoint) = std::env::var("OTEL_EXPORTER_OTLP_ENDPOINT") {
-        if endpoint.starts_with("https") {
-            exporter = exporter.with_tls_config(Default::default());
-        }
-    }
-
-    let tracer = opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_exporter(exporter)
-        .install_batch(opentelemetry::runtime::Tokio)
-        .unwrap();
-
-    let telemetry_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+    let telemetry_layer = tracing_opentelemetry::layer().with_tracer(create_otlp_tracer());
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::from_default_env())
@@ -49,31 +31,66 @@ async fn main() -> io::Result<()> {
     server.workers(2).run().await
 }
 
-fn load_metadata_from_env() -> tonic::metadata::MetadataMap {
+fn create_otlp_tracer() -> opentelemetry::sdk::trace::Tracer {
+    let protocol = std::env::var("OTEL_EXPORTER_OTLP_PROTOCOL").unwrap_or("grpc".to_string());
+
+    let mut tracer = opentelemetry_otlp::new_pipeline().tracing();
+    let headers = parse_otlp_headers_from_env();
+
+    match protocol.as_str() {
+        "grpc" => {
+            let mut exporter = opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_metadata(metadata_from_headers(headers))
+                .with_env();
+
+            // Check if we need TLS
+            if let Ok(endpoint) = std::env::var("OTEL_EXPORTER_OTLP_ENDPOINT") {
+                if endpoint.starts_with("https") {
+                    exporter = exporter.with_tls_config(Default::default());
+                }
+            }
+            tracer = tracer.with_exporter(exporter)
+        }
+        "http/proto" => {
+            let exporter = opentelemetry_otlp::new_exporter()
+                .http()
+                .with_headers(headers.into_iter().collect())
+                .with_env();
+            tracer = tracer.with_exporter(exporter)
+        }
+        p => panic!("Unsupported protocol {}", p),
+    };
+
+    tracer.install_batch(opentelemetry::runtime::Tokio).unwrap()
+}
+
+fn metadata_from_headers(headers: Vec<(String, String)>) -> tonic::metadata::MetadataMap {
     use tonic::metadata;
 
     let mut metadata = metadata::MetadataMap::new();
-    if let Ok(headers) = std::env::var("OTEL_EXPORTER_OTLP_HEADERS") {
-        headers
-            .split(',')
+    headers.into_iter().for_each(|(name, value)| {
+        let value = value
+            .parse::<metadata::MetadataValue<metadata::Ascii>>()
+            .expect("Header value invalid");
+        metadata.insert(metadata::MetadataKey::from_str(&name).unwrap(), value);
+    });
+    metadata
+}
+
+fn parse_otlp_headers_from_env() -> Vec<(String, String)> {
+    let mut headers = Vec::new();
+
+    if let Ok(hdrs) = std::env::var("OTEL_EXPORTER_OTLP_HEADERS") {
+        hdrs.split(',')
             .map(|header| {
                 header
                     .split_once('=')
                     .expect("Header should contain '=' character")
             })
-            .map(|(name, value)| {
-                (
-                    name,
-                    value
-                        .parse::<metadata::MetadataValue<metadata::Ascii>>()
-                        .expect("Header value invalid"),
-                )
-            })
-            .for_each(|(name, value)| {
-                metadata.insert(metadata::MetadataKey::from_str(name).unwrap(), value);
-            });
+            .for_each(|(name, value)| headers.push((name.to_owned(), value.to_owned())));
     }
-    metadata
+    headers
 }
 
 #[tracing::instrument()]
